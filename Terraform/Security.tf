fae174c7889bd6c67851345a00a674158bc33758
kovvuridharani <69035971+kovvuridharani@users.noreply.github.com>
2025-03-03 12:51:30 +0530

    Update Security.tf

--- Terraform/Security.tf
+++ Terraform/Security.tf
@@ -8,7 +8,7 @@ resource "aws_security_group_rule" "Cluster_Grouprule" {
   from_port         = 0
   protocol          = "-1"
   to_port           = 0
-  security_group_id = aws_security_group.all_worker_mgmt.id
+  security_group_id = aws_security_group.Cluster_Security.id
   type              = "ingress"
   cidr_blocks = [
     "10.0.0.0/8",
@@ -21,7 +21,7 @@ resource "aws_security_group_rule" "Clutser_id" {
   description       = "allow outbound traffic to anywhere"
   from_port         = 0
   protocol          = "-1"
-  security_group_id = aws_security_group.all_worker_mgmt.id
+  security_group_id = aws_security_group.Cluster_Security.id
   to_port           = 0
   type              = "egress"
   cidr_blocks       = ["0.0.0.0/0"]
