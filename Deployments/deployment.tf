6afa6b6fe0b697b36acfaed5a5d2124168b929d9
kovvuridharani <69035971+kovvuridharani@users.noreply.github.com>
2025-03-04 10:18:52 +0530

    Create deployment.tf

---
+++ Deployments/deployment.tf
@@ -0,0 +1,41 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "kubernetes_deployment" "Nginix" {
+  metadata {
+    name = "nginx-deployment"
+    labels = {
+      app = "nginx"
+    }
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          name  = "nginx"
+          image = "nginx:latest"
+
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
